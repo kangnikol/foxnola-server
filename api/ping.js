6fe1f5a1a5aece5c568996fac0d7456142fb050f
kangnikol <nicholasalvisaputra@gmail.com>
2024-11-21 14:22:47 +0700

    fix

--- api/ping.js
+++ api/ping.js
@@ -3,8 +3,9 @@ import { MinecraftServer } from "mcping-js"
 export default function handler(req, res) {
   const server = new MinecraftServer("FoxNola.aternos.me", 64523)
 
-  server.ping(4000, 768, (err, response) => {
+  server.ping(10000, -1, (err, response) => {
     if (err) {
+      console.error("Ping Error:", err)
       return res
         .status(500)
         .json({ error: "Error pinging server", details: err.message })
