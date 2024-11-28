0cc1f3218016a07a7fd5d9b7e8e135c4202ce3c7
kangnikol <nicholasalvisaputra@gmail.com>
2024-11-28 15:00:08 +0700

    backkk

--- api/ping.js
+++ api/ping.js
@@ -1,28 +1,16 @@
 import { MinecraftServer } from "mcping-js"
 
-const serverConfig = {
-  address: "NgobrolSantai.aternos.me",
-  port: 64523,
-  timeout: 10000,
-  protocolVersion: 768,
-}
+export default async function handler(req, res) {
+  try {
+    const server = new MinecraftServer("NgobrolSantai.aternos.me", 64523)
+    const response = await server.ping(1000, 768)
 
-export default function handler(req, res) {
-  const server = new MinecraftServer(serverConfig.address, serverConfig.port)
-
-  server.ping(
-    serverConfig.timeout,
-    serverConfig.protocolVersion,
-    (err, response) => {
-      if (err) {
-        console.error("Ping Error:", err)
-        return res
-          .status(500)
-          .json({ error: "Error pinging server", details: err.message })
-      }
-
-      console.log("Ping Response:", response)
-      res.json(response)
-    }
-  )
+    console.log("Ping Response:", response) // Log the response to verify its structure
+    res.json(response)
+  } catch (err) {
+    console.error("Ping Error:", err)
+    res
+      .status(500)
+      .json({ error: "Error pinging server", details: err.message })
+  }
 }
